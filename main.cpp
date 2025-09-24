00c9786f2c14c29135feb31f144b3f3593b20602
angel.java <2430124@upv.edu.mx>
2025-09-24 09:41:39 -0600

    Cambio minimo (se cambió el nombre de la ferreteria y se minimizaron las lineas de comentarios)

--- main.cpp
+++ main.cpp
@@ -6,16 +6,14 @@ void actualizarStock(int codigos[], std::string nombres[], int stock[], int tam)
 void generarReporte(int codigos[], std::string nombres[], int stock[], float precios[], int tam);
 void buscarProductoMasCaro(std::string nombres[], float precios[], int tam);
 
-/*
-La función main tiene las variables int de opcion_selec (la cual recibe el resultado de mostrarMenu()),
-tam (los productos proedefinidos, con el proposito de no recorrer los otros 95 indices vaciaas de los arrays)
+/* La función main tiene las variables int de opcion_selec (la cual recibe el resultado de mostrarMenu()),
+tam (los productos proedefinidos, con el proposito de no recorrer los otros 95 indices vacios de los arrays)
 y tam_max (tamaño total de los arreglos). Así como los 4 arrays paralelos: codigos, nombres, stock y precios,
 cada uno de ellos con 5 datos predefinidos.
 
 El while principal del sistema es aquel en donde opcion_selec recibe la opción seleccionada en el menú que,
 mientras no sea 5 (los valores mayores a 5 o menores a 1 ya se encuentran validados en la funcion de mostrarMenu()),
-se seguira ejecutando en bucle, llamando a las funciones correspondientes en cada caso con la estructura switch.
-*/
+se seguira ejecutando en bucle, llamando a las funciones correspondientes en cada caso con la estructura switch. */
 
 int main() {
     int opcion_selec = 0, tam = 5, tam_max = 100;
@@ -50,26 +48,24 @@ int main() {
     return 0;
 }
 
-/*
-Esta función no recibe ningun parametro pero si devuelve la elección que el usuario ingreso de las
+/* Esta función no recibe ningun parametro pero si devuelve la elección que el usuario ingreso de las
 opciones del menú, la eleccion se almacena en la variable opcion, el menú se imprime mientras que 
 la opción selecionada no entre entre 1 y 5, mostrandole un mensaje claro al usuario de que debe 
 ingresar una opción válida, al final, cuando se ingresa una opción válida, se retorna la elección 
-del usuario. 
-*/
+del usuario. */
 
 int mostrarMenu(){
     int opcion = 0;
     bool inputValido = false;
     
     while (!inputValido || opcion < 1 || opcion > 5){
-        std::cout << "--- Ferreteria 'Pablito clavo un clavito' ---" << std::endl;
+        std::cout << "--- Ferreteria 'El Martillo' ---" << std::endl;
         std::cout << "1. Consultar un producto" << std::endl;
         std::cout << "2. Actualizar inventario" << std::endl;
         std::cout << "3. Generar reporte completo" << std::endl;
         std::cout << "4. Encontrar el producto mas caro" << std::endl;
         std::cout << "5. Salir" << std::endl;
-        std::cout << "---------------------------------------------" << std::endl;
+        std::cout << "--------------------------------" << std::endl;
         std::cout << "Digite una opcion: ";
 
         if (std::cin >> opcion){
@@ -92,16 +88,14 @@ int mostrarMenu(){
     return opcion;
 }
 
-/*
-Esta función recibe como argumentos los 4 arrays paralelos, asi como el numero de productos actual o indices 
+/* Esta función recibe como argumentos los 4 arrays paralelos, asi como el numero de productos actual o indices 
 utilizados actualmente en los arrays, se definen las variables de ingreso de dato del codigo que busca el usuario
 llamada codigo_a_buscar, la bandera de si se encontró el código ingresado y el indice en que se encontró. 
 
 Se recorre el array de codigos, si no existe dicho producto, se le notifica al usuario, por otro lado, si
 existe se guarda el indice. Se evalua la bandera encontrado, si es false, se le muestra un mensaje claro al 
 usuario, si es true, se proceden a mostrar los datos de los aarrays paralelos en el indice en que se encontró
-el código.
-*/
+el código. */
 
 void consultarProducto(int codigos[], std::string nombres[], int stock[], float precios[], int tam){
     float encontrado = false;
@@ -144,8 +138,7 @@ void consultarProducto(int codigos[], std::string nombres[], int stock[], float 
     }
 }
 
-/*
-Esta función recibe como argumentos los arrays paralelos de codigos, nombres y stock, asi como
+/* Esta función recibe como argumentos los arrays paralelos de codigos, nombres y stock, asi como
 el numero de productos actual o indices utilizados actualmente en los arrays, se definen las variables
 de ingreso de dato del codigo que busca el usuario llamada codigo_a_buscar, la bandera de si se encontró
 el código ingresado, el indice en que se encontró y la cantidad con la que se actualiza el stock. 
@@ -155,8 +148,7 @@ existe se guarda el indice y la variable de cant_p_actualizar se modifica a un v
 condición de que la suma de mayor o igual a 0, esto para asegurarse de que el usuario siempre ingrese una
 cantidad válida y el stock nunca sea menor que 0, por lo que se le pedira una cantidaad adecuada hasta que 
 el stock no resulte negativo, se le muestra el error y se le vuelve a pedir el dato. Al final, cuando el stock 
-se actualice a un valor válido se aplica a el array stock en el indice encontrado.
-*/
+se actualice a un valor válido se aplica a el array stock en el indice encontrado. */
 
 void actualizarStock(int codigos[], std::string nombres[], int stock[], int tam){
     float encontrado = false;
@@ -225,13 +217,11 @@ void actualizarStock(int codigos[], std::string nombres[], int stock[], int tam)
     }
 }
 
-/*
-Esta función recibe como argumentos los 4 arrays paralelos y la cantidad de productos actualmente (solo 5 de los 100 
+/* Esta función recibe como argumentos los 4 arrays paralelos y la cantidad de productos actualmente (solo 5 de los 100 
 indices de los arrays se utilizan).
 
 Se recorren los 4 arrays desde el indice 0 hasta que sea menor a tam (menor a 5), mientras tanto, se imprimen los
-datos de los 4 arrays en cada indice, con el fin de mostrar todos los productos y sus detalles.
-*/
+datos de los 4 arrays en cada indice, con el fin de mostrar todos los productos y sus detalles. */
 
 void generarReporte(int codigos[], std::string nombres[], int stock[], float precios[], int tam){
 
@@ -245,13 +235,11 @@ void generarReporte(int codigos[], std::string nombres[], int stock[], float pre
     std::cout << "---------------------------------------" << std::endl;
 }
 
-/*
-Esta función recibe como argumentos los arrays de nombre y precios, además la cantidad de productos actualmente.
+/* Esta función recibe como argumentos los arrays de nombre y precios, además la cantidad de productos actualmente.
 
 Se recorre el array de precio y se compara con la variable de precioMasAlto, la cual va almacenando el más
 alto que se vaya encontrando, es decir, si se encuntrea uno mayor al anteriormente guardado entonces se reemplaza, 
-se almacenan en las variable precioMasAlto y nombreProductoMasCaro, al final se muestra el producto más caro en un mensaje.
-*/
+se almacenan en las variable precioMasAlto y nombreProductoMasCaro, al final se muestra el producto más caro en un mensaje. */
 
 void buscarProductoMasCaro(std::string nombres[], float precios[], int tam){
     float precioMasAlto = 0.00;
